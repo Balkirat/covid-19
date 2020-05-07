48c1ed6f0b6ec086d572d58b6ccdf64e21b613b0
Balkirat <cheemaballu@gmail.com>
2020-05-07 19:11:29 -0400

    added link to learn more button

--- src/Components/Hero/hero.js
+++ src/Components/Hero/hero.js
@@ -10,11 +10,15 @@ function hero() {
       <Jumbotron>
         <h1>COVID-19</h1>
         <p>
-        Corona Virus is, unfortunately, spreading through the entire world. As a Developer I just thought to make a Simple webpage to 
-        spread awareness by visualizing the data in the form of charts and simple text tables.
+          Corona Virus is, unfortunately, spreading through the entire world. As
+          a Developer I just thought to make a Simple webpage to spread
+          awareness by visualizing the data in the form of charts and simple
+          text tables.
         </p>
         <p>
-          <Button variant="primary">Learn more</Button>
+          <a target="_blank" href="https://www.canada.ca/en/public-health/services/diseases/coronavirus-disease-covid-19.html?utm_campaign=not-applicable&utm_medium=vanity-url&utm_source=canada-ca_coronavirus">
+            <Button variant="primary">Learn more</Button>
+          </a>
         </p>
       </Jumbotron>
     </>
